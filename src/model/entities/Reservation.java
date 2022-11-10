3a8d82e4f27cf0fd8d132f7009de5c05288ffc56
Anderson <andersson_73@live.com>
2022-11-10 14:44:42 -0300

    Bad solutuion

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -41,9 +41,17 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updtadeDates(Date checkIn, Date checkOut) {
+	public String updtadeDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Rreservation dates for updates must be future dates";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
